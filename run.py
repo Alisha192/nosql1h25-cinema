d5c05b341aa3251151e61d5a7f6e39706ffa6e0a
Dmitry Timchenko <dmitriy.timchenko@icloud.com>
2025-05-05 23:42:17 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         # Создание индексов
+
         app.mongo.db.film.create_index([("title", "text")])
         app.mongo.db.person.create_index([("name", "text")])
         app.mongo.db.admin.create_index([("login", 1)], unique=True)
